a0ec41ac73308d6e118caec3d350cbaf73937c32
Sammybett-bit <bettsammy20@gmail.com>
2023-11-06 00:08:38 +0300

    update donation

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,187 +1,12 @@
-// import React, { useState } from 'react';
-// import './Donation.css'
-
-// const Donation = () => {
-//   const [selectedOption, setSelectedOption] = useState('food');
-//   const [phoneNumber, setPhoneNumber] = useState('');
-//   const [donationAmount, setDonationAmount] = useState('10');
-
-//   const handleOptionChange = (event) => {
-//     setSelectedOption(event.target.value);
-//   };
-
-//   const handlePhoneNumberChange = (event) => {
-//     setPhoneNumber(event.target.value);
-//   };
-
-//   const handleDonationAmountChange = (event) => {
-//     setDonationAmount(event.target.value);
-//   };
-
-//   const handleDonation = () => {
-//     const donationData = {
-//       option: selectedOption,
-//       phoneNumber: phoneNumber,
-//       donationAmount: donationAmount,
-//     };
-
-//     fetch('https://charity-0d32.onrender.com/donations', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(donationData),
-//     })
-//       .then((response) => {
-//         if (response.ok) {
-//           console.log('Donation submitted successfully');
-//         } else {
-//           console.error('Failed to submit donation');
-//         }
-//       })
-//       .catch((error) => {
-//         console.error('Error:', error);
-//       });
-//   };
-
-//   return (
-//     <div className="donation-form">
-//       <h2>Donate Now</h2>
-//       <label>Select Donation Option:</label>
-//       <select
-//         className="donation-select"
-//         value={selectedOption}
-//         onChange={handleOptionChange}
-//       >
-//         <option value="food">Food</option>
-//         <option value="clothing">Clothing</option>
-//         <option value="fees">Fees</option>
-//         <option value="medication">Medication</option>
-//         <option value="shelter">Shelter</option>
-//         <option value="others">Others</option>
-//       </select>
-
-//       <label>Donation Amount:</label>
-//       <input
-//         className="donation-input"
-//         type="number"
-//         value={donationAmount}
-//         onChange={handleDonationAmountChange}
-//         placeholder="Enter donation amount"
-//       />
-
-//       <label>Phone Number:</label>
-//       <input
-//         className="donation-input"
-//         type="text"
-//         value={phoneNumber}
-//         onChange={handlePhoneNumberChange}
-//         placeholder="Enter your phone number"
-//       />
-
-//       <button className="donation-button" onClick={handleDonation}>
-//         Donate
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Donation;
-
-// import React, { useState } from 'react';
-// import axios from 'axios'; // Import Axios
-// import './Donation.css';
-// const [selectedOption, setSelectedOption] = useState('food');
-//   const [phoneNumber, setPhoneNumber] = useState('');
-//   const [donationAmount, setDonationAmount] = useState('10');
-
-//   const handleOptionChange = (event) => {
-//     setSelectedOption(event.target.value);
-//   };
-
-//   const handlePhoneNumberChange = (event) => {
-//     setPhoneNumber(event.target.value);
-//   };
-
-//   const handleDonationAmountChange = (event) => {
-//     setDonationAmount(event.target.value);
-//   };
-
-//   const handleDonation = () => {
-//     const donationData = {
-//       option: selectedOption,
-//       phoneNumber: phoneNumber,
-//       donationAmount: donationAmount,
-//     };
-
-//     axios
-//       .post('https://charity-0d32.onrender.com/donations', donationData, {
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       })
-//       .then((response) => {
-//         console.log('Donation submitted successfully');
-//       })
-//       .catch((error) => {
-//         console.error('Failed to submit donation:', error);
-//       });
-//   };
-
-//   return (
-//     <div className="donation-form">
-//       <h2>Donate Now</h2>
-//       <label>Select Donation Option:</label>
-//       <select
-//         className="donation-select"
-//         value={selectedOption}
-//         onChange={handleOptionChange}
-//       >
-//         <option value="food">Food</option>
-//         <option value="clothing">Clothing</option>
-//         <option value="fees">Fees</option>
-//         <option value="medication">Medication</option>
-//         <option value="shelter">Shelter</option>
-//         <option value="others">Others</option>
-//       </select>
-
-//       <label>Donation Amount:</label>
-//       <input
-//         className="donation-input"
-//         type="number"
-//         value={donationAmount}
-//         onChange={handleDonationAmountChange}
-//         placeholder="Enter donation amount"
-//       />
-
-//       <label>Phone Number:</label>
-//       <input
-//         className="donation-input"
-//         type="text"
-//         value={phoneNumber}
-//         onChange={handlePhoneNumberChange}
-//         placeholder="Enter your phone number"
-//       />
-
-//       <button className="donation-button" onClick={handleDonation}>
-//         Donate
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Donation;
-// const Donation = () => {
-  
-
 import React, { useState } from 'react';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
 import './Donation.css';
 
 const Donation = () => {
   const [selectedOption, setSelectedOption] = useState('food');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [donationAmount, setDonationAmount] = useState('10');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -196,37 +21,35 @@ const Donation = () => {
   };
 
   const handleDonation = async () => {
-    const formData = {
+    const donationData = {
       option: selectedOption,
       phoneNumber: phoneNumber,
       donationAmount: donationAmount,
     };
 
     try {
-      const response = await axios.post('https://charity-0d32.onrender.com/donations', formData, {
+      const response = await axios.post('https://charity-0d32.onrender.com/donations', donationData, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
 
-      // Additional data to send after the API call
-      const additionalData = {
-        options: formData.option,
-        amount: formData.donationAmount,
-        phonenumber: formData.phoneNumber,
-      };
-
-      // You can now do something with the additional data or log it
-      console.log('Donation submitted successfully');
-      console.log('Additional data:', additionalData);
+      if (response.status === 201) {
+        console.log('Donation submitted successfully');
+        // Optionally, reset the form or show a success message.
+      } else {
+        setErrorMessage('Failed to submit donation. Please try again.');
+      }
     } catch (error) {
       console.error('Failed to submit donation:', error);
+      setErrorMessage('Failed to submit donation. Please try again.');
     }
   };
 
   return (
     <div className="donation-form">
       <h2>Donate Now</h2>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <label>Select Donation Option:</label>
       <select
         className="donation-select"
